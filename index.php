916063e1544c09ab62e565085bc2fed7b4e7cc3e
aleksnavarro <jairo.navarros@alumnos.udg.mx>
2019-02-22 21:55:11 -0600

    primer commit

---
+++ index.php
@@ -0,0 +1,1 @@
+<?php header( 'Location: /part1.html' ); ?>
---
+++ js/main_step1.js
@@ -0,0 +1,305 @@
+//COLORS
+var Colors = {
+    red:0xf25346,
+    white:0xd8d0d1,
+    brown:0x59332e,
+    pink:0xF5986E,
+    brownDark:0x23190f,
+    blue:0x68c3c0,
+    green:0x07c41a,
+    lightorange:0xfcd32d,
+    black:0x000000
+};
+
+// THREEJS RELATED VARIABLES
+
+var scene,
+    camera, fieldOfView, aspectRatio, nearPlane, farPlane,
+    renderer, container;
+
+//SCREEN & MOUSE VARIABLES
+
+var HEIGHT, WIDTH,
+    mousePos = { x: 0, y: 0 };
+
+//INIT THREE JS, SCREEN AND MOUSE EVENTS
+
+function createScene() {
+
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+
+  scene = new THREE.Scene();
+  aspectRatio = WIDTH / HEIGHT;
+  fieldOfView = 60;
+  nearPlane = 1;
+  farPlane = 10000;
+  camera = new THREE.PerspectiveCamera(
+    fieldOfView,
+    aspectRatio,
+    nearPlane,
+    farPlane
+    );
+  scene.fog = new THREE.Fog(0xf7d9aa, 100,950);
+  camera.position.x = 0;
+  camera.position.z = 200;
+  camera.position.y = 100;
+
+  renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+  renderer.setSize(WIDTH, HEIGHT);
+  renderer.shadowMap.enabled = true;
+  container = document.getElementById('world');
+  container.appendChild(renderer.domElement);
+
+  window.addEventListener('resize', handleWindowResize, false);
+}
+
+// HANDLE SCREEN EVENTS
+
+function handleWindowResize() {
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+  renderer.setSize(WIDTH, HEIGHT);
+  camera.aspect = WIDTH / HEIGHT;
+  camera.updateProjectionMatrix();
+}
+
+
+// LIGHTS
+
+var ambientLight, hemisphereLight, shadowLight;
+
+function createLights() {
+
+  hemisphereLight = new THREE.HemisphereLight(0xaaaaaa,0x000000, .9)
+  shadowLight = new THREE.DirectionalLight(0xffffff, .9);
+  shadowLight.position.set(150, 350, 350);
+  shadowLight.castShadow = true;
+  shadowLight.shadow.camera.left = -400;
+  shadowLight.shadow.camera.right = 400;
+  shadowLight.shadow.camera.top = 400;
+  shadowLight.shadow.camera.bottom = -400;
+  shadowLight.shadow.camera.near = 1;
+  shadowLight.shadow.camera.far = 1000;
+  shadowLight.shadow.mapSize.width = 2048;
+  shadowLight.shadow.mapSize.height = 2048;
+
+  scene.add(hemisphereLight);
+  scene.add(shadowLight);
+}
+
+
+var AirPlane = function(){
+  this.mesh = new THREE.Object3D();
+  this.mesh.name = "airPlane";
+
+  // Create the body
+  var geomCockpit = new THREE.BoxGeometry(60,50,50,1,1,1);
+  var matCockpit = new THREE.MeshPhongMaterial({color:Colors.white, shading:THREE.FlatShading});
+  var cockpit = new THREE.Mesh(geomCockpit, matCockpit);
+  cockpit.castShadow = true;
+  cockpit.receiveShadow = true;
+  this.mesh.add(cockpit);
+
+  // Create Wing
+  var geomSideWing = new THREE.BoxGeometry(40,40,70,1,1,1);
+  var matSideWing = new THREE.MeshPhongMaterial({color:Colors.white, shading:THREE.FlatShading});
+  var sideWing = new THREE.Mesh(geomSideWing, matSideWing);
+  sideWing.position.set(10,5,0);
+  sideWing.castShadow = true;
+  sideWing.receiveShadow = true;
+  this.mesh.add(sideWing);
+
+  // Create the head
+  var geomHead = new THREE.BoxGeometry(30,50,15,1,1,1);
+  var matHead = new THREE.MeshPhongMaterial({color:Colors.white, shading:THREE.FlatShading});
+  var Head = new THREE.Mesh(geomHead, matHead);
+  Head.position.set(30,30,0);
+  Head.castShadow = true;
+  Head.receiveShadow = true;
+  this.mesh.add(Head);
+
+  // Create the lips
+  var geomLips = new THREE.BoxGeometry(10,10,5);
+  var matLips = new THREE.MeshPhongMaterial({color:Colors.lightorange, shading:THREE.FlatShading});
+  var Lips = new THREE.Mesh(geomLips, matLips);
+  Lips.position.set(45,35,0);
+  Lips.castShadow = true;
+  Lips.receiveShadow = true;
+  this.mesh.add(Lips);
+
+  // Create the eyes
+  var geomEye = new THREE.BoxGeometry(5,5,20,1,1,1);
+  var matEye = new THREE.MeshPhongMaterial({color:Colors.black, shading:THREE.FlatShading});
+  var Eye = new THREE.Mesh(geomEye, matEye);
+  Eye.position.set(35,40,0);
+  Eye.castShadow = true;
+  Eye.receiveShadow = true;
+  this.mesh.add(Eye);
+
+};
+
+Sky = function(){
+  this.mesh = new THREE.Object3D();
+  this.nClouds = 20;
+  this.clouds = [];
+  var stepAngle = Math.PI*2 / this.nClouds;
+  for(var i=0; i<this.nClouds; i++){
+    var c = new Cloud();
+    this.clouds.push(c);
+    var a = stepAngle*i;
+    var h = 750 + Math.random()*200;
+    c.mesh.position.y = Math.sin(a)*h;
+    c.mesh.position.x = Math.cos(a)*h;
+    c.mesh.position.z = -400-Math.random()*400;
+    c.mesh.rotation.z = a + Math.PI/2;
+    var s = 1+Math.random()*2;
+    c.mesh.scale.set(s,s,s);
+    this.mesh.add(c.mesh);
+  }
+}
+
+Sea = function(){
+  var geom = new THREE.CylinderGeometry(600,600,800,40,10);
+  geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
+  geom.mergeVertices();
+  var l = geom.vertices.length;
+
+  this.waves = [];
+
+  for (var i=0;i<l;i++){
+    var v = geom.vertices[i];
+    this.waves.push({y:v.y,
+      x:v.x,
+      z:v.z,
+      ang:Math.random()*Math.PI*2,
+      amp:5 + Math.random()*15,
+      speed:0.016 + Math.random()*0.032
+    });
+  };
+  var mat = new THREE.MeshPhongMaterial({
+    color:Colors.green,
+    transparent:true,
+    opacity:.8,
+    shading:THREE.FlatShading,
+
+  });
+
+  this.mesh = new THREE.Mesh(geom, mat);
+  this.mesh.receiveShadow = true;
+
+}
+
+Sea.prototype.moveWaves = function (){
+  var verts = this.mesh.geometry.vertices;
+  var l = verts.length;
+  for (var i=0; i<l; i++){
+    var v = verts[i];
+    var vprops = this.waves[i];
+    v.x =  vprops.x + Math.cos(vprops.ang)*vprops.amp;
+    v.y = vprops.y + Math.sin(vprops.ang)*vprops.amp;
+    vprops.ang += vprops.speed;
+  }
+  this.mesh.geometry.verticesNeedUpdate=true;
+  sea.mesh.rotation.z += .005;
+}
+
+Cloud = function(){
+  this.mesh = new THREE.Object3D();
+  this.mesh.name = "cloud";
+  var geom = new THREE.SphereGeometry(20,20,20);
+  var mat = new THREE.MeshPhongMaterial({
+    color:Colors.white,
+  });
+
+  var nBlocs = 3+Math.floor(Math.random()*3);
+  for (var i=0; i<nBlocs; i++ ){
+    var m = new THREE.Mesh(geom.clone(), mat);
+    m.position.x = i*15;
+    m.position.y = Math.random()*10;
+    m.position.z = Math.random()*10;
+    m.rotation.z = Math.random()*Math.PI*2;
+    m.rotation.y = Math.random()*Math.PI*2;
+    var s = .1 + Math.random()*.9;
+    m.scale.set(s,s,s);
+    m.castShadow = true;
+    m.receiveShadow = true;
+    this.mesh.add(m);
+  }
+}
+
+// 3D Models
+var sea;
+var airplane;
+
+function createPlane(){
+  airplane = new AirPlane();
+  airplane.mesh.scale.set(.25,.25,.25);
+  airplane.mesh.position.y = 100;
+  scene.add(airplane.mesh);
+}
+
+function createSea(){
+  sea = new Sea();
+  sea.mesh.position.y = -600;
+  scene.add(sea.mesh);
+}
+
+function createSky(){
+  sky = new Sky();
+  sky.mesh.position.y = -600;
+  scene.add(sky.mesh);
+}
+
+function loop(){
+  updatePlane();
+  sea.moveWaves();
+  sea.mesh.rotation.z += .005;
+  sky.mesh.rotation.z += .01;
+  renderer.render(scene, camera);
+  requestAnimationFrame(loop);
+}
+
+function updatePlane(){
+  var targetY = normalize(mousePos.y,-.75,.75,25, 175);
+  var targetX = normalize(mousePos.x,-.75,.75,-100, 100);
+  airplane.mesh.position.y += (targetY-airplane.mesh.position.y)*0.1;
+  airplane.mesh.rotation.z = (targetY-airplane.mesh.position.y)*0.0128;
+  airplane.mesh.rotation.x = (airplane.mesh.position.y-targetY)*0.0064;
+}
+
+function normalize(v,vmin,vmax,tmin, tmax){
+  var nv = Math.max(Math.min(v,vmax), vmin);
+  var dv = vmax-vmin;
+  var pc = (nv-vmin)/dv;
+  var dt = tmax-tmin;
+  var tv = tmin + (pc*dt);
+  return tv;
+}
+
+function updateCameraFov(){
+  camera.fov = normalize(mousePos.x,-1,1,40, 80);
+  camera.updateProjectionMatrix();
+}
+
+function init(event){
+  document.addEventListener('mousemove', handleMouseMove, false);
+  createScene();
+  createLights();
+  createPlane();
+  createSea();
+  createSky();
+  loop();
+}
+
+// HANDLE MOUSE EVENTS
+
+var mousePos = { x: 0, y: 0 };
+
+function handleMouseMove(event) {
+  var tx = -1 + (event.clientX / WIDTH)*2;
+  var ty = 1 - (event.clientY / HEIGHT)*2;
+  mousePos = {x:tx, y:ty};
+}
+
+window.addEventListener('load', init, false);
